68aa469915632019dd7306717dcd7549974b1fb9
DagmawiAbate <dagmawiabate2@gmail.com>
2022-04-29 15:19:44 +0300

    Finishing up

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,26 +1,29 @@
 <template>
-  <h1>{{ job.title }}</h1>
-  <p>This job id is {{ id }}</p>
-  <h1>{{ job.details }}</h1>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>This job id is {{ id }}</p>
+    <h1>{{ job.details }}</h1>
+  </div>
+  <div v-else>
+    <p>Loading job detalils...</p>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['id'],
+  props: ["id"],
   data() {
     return {
-      job: null
+      job: null,
     }
   },
   mounted() {
-    fetch('http://localhost:3000/jobs/' + this.id)
-      .then(res => res.json())
-      .then(data => this.jobs = data)
-      .catch(err => console.log(err.message))
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
